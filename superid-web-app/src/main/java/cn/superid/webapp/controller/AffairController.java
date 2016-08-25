0d8717d6a340e46f9d06c45ec18f1fbce9066677
xmc1993 <1170143845@qq.com>
2016-08-25 16:06:45 +0800

    swagger

--- superid-web-app/src/main/java/cn/superid/webapp/controller/AffairController.java
+++ superid-web-app/src/main/java/cn/superid/webapp/controller/AffairController.java
@@ -4,6 +4,7 @@ import cn.superid.webapp.annotation.RequiredPermissions;
 import cn.superid.webapp.forms.SimpleResponse;
 import cn.superid.webapp.security.AffairPermissions;
 import cn.superid.webapp.service.IAffairService;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,9 @@ public class AffairController {
     @Autowired
     private IAffairService affairService;
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
+
+    @ApiOperation(value = "创建事务", httpMethod = "POST", response = SimpleResponse.class, notes = "创建一个新的事务")
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
     @RequiredPermissions(affair = {AffairPermissions.AddAffair,AffairPermissions.AddNotice})
     public SimpleResponse CreateAffair(HttpServletRequest request, String token){
 
---
+++ superid-web-app/src/main/java/cn/superid/webapp/swagger/MySwaggerConfig.java
@@ -0,0 +1,44 @@
+package cn.superid.webapp.swagger;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by xmc1993 on 16/8/25.
+ */
+@Configuration
+@EnableSwagger
+public class MySwaggerConfig {
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig)
+    {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation()
+    {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(
+                ".*?");
+    }
+
+    private ApiInfo apiInfo()
+    {
+        ApiInfo apiInfo = new ApiInfo(
+                "SuperId",//My Apps API Title
+                "SuperId接口文档",//My Apps API Description
+                "My Apps API terms of service",
+                "My Apps API Contact Email",
+                "My Apps API Licence Type",
+                "My Apps API License URL");
+        return apiInfo;
+    }
+
+}
