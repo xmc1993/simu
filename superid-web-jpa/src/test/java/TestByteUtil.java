9e78765f4f4c1ed80f5150b630e13ca3e4b6fcdb
jizhenya <844094462@qq.com>
2016-09-05 16:25:31 +0800

    bug

--- superid-web-jpa/src/test/java/TestByteUtil.java
+++ superid-web-jpa/src/test/java/TestByteUtil.java
@@ -17,17 +17,6 @@ public class TestByteUtil {
 
     @Test
     public void getData(){
-        System.out.println("long");
-        System.out.println(Long.BYTES);
-        System.out.println("int");
-        System.out.println(Integer.BYTES);
-        System.out.println("double");
-        System.out.println(Double.BYTES);
-        System.out.println("float");
-        System.out.println(Float.BYTES);
-        System.out.println("character");
-        System.out.println(Character.BYTES);
-        System.out.println("short");
-        System.out.println(Short.BYTES);
+
     }
 }
