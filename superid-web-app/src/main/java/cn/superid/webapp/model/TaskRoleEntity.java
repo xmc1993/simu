7fe2abb783ef0de68f5a288f2f7d58049eebde30
jizhenya <844094462@qq.com>
2016-09-07 17:04:18 +0800

    task实体类

---
+++ superid-web-app/src/main/java/cn/superid/webapp/model/TaskRoleEntity.java
@@ -0,0 +1,47 @@
+package cn.superid.webapp.model;
+
+import cn.superid.jpa.orm.Dao;
+import cn.superid.jpa.orm.ExecutableModel;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by jizhenya on 16/9/7.
+ */
+@Table(name = "task_role")
+public class TaskRoleEntity extends ExecutableModel {
+
+    public final static Dao<TaskRoleEntity> dao = new Dao<>(TaskRoleEntity.class);
+
+    private Long id;
+    private Long taskId;
+    private Long roleId;
+
+    @Id
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+}
