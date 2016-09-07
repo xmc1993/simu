f0f48b6b33749bce3e8d91114a0ea6abe2fb8987
starking <15951818230@163.com>
2016-09-07 14:22:36 +0800

    tmp

--- superid-web-app/src/test/java/UserServiceTest.java
+++ superid-web-app/src/test/java/UserServiceTest.java
@@ -1,5 +1,8 @@
 import cn.superid.webapp.controller.UserController;
+import cn.superid.webapp.forms.EditUserBaseInfo;
 import cn.superid.webapp.model.UserEntity;
+import cn.superid.webapp.model.base.UserBaseInfo;
+import cn.superid.webapp.security.IAuth;
 import cn.superid.webapp.service.IUserService;
 import cn.superid.webapp.utils.AliSmsDao;
 import cn.superid.webapp.utils.PasswordEncryptor;
@@ -28,19 +31,37 @@ public class UserServiceTest{
     @Autowired
     private IUserService userService;
     @Autowired
+    private IAuth auth;
+    @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @Test
-    public void testCreateUser(){
+    private UserEntity addUser(){
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername("大哥鹏");
         userEntity.setPassword(PasswordEncryptor.encode("123456"));
         userEntity.setMobile("15951818231");
         UserEntity result = userService.createUser(userEntity);
+        return  result;
+    }
+
+    @Test
+    public void testCreateUser(){
+        UserEntity result = addUser();
         Assert.assertFalse(result==null);
 
     }
 
+    @Test
+     public void testEditInfo(){
+        UserEntity testUser = addUser();
+        auth.authUser(testUser.getId());
+        EditUserBaseInfo editUserBaseInfo = new EditUserBaseInfo();
+        editUserBaseInfo.setAvatar("test");
+        userService.editBaseInfo(editUserBaseInfo);
+        UserBaseInfo userBaseInfo=UserBaseInfo.dao.findById(testUser.getId());
+        Assert.assertTrue(userBaseInfo.getAvatar().equals("test"));
+
+    }
 
 
 
