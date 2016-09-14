f8392a8cc215be41c108c4f139c681c851ef61b4
njuTms <tms@superid.cn>
2016-09-14 12:23:22 +0800

    新增了一些盟和事务的service功能点,修改了model的bug

--- superid-web-app/src/main/java/cn/superid/webapp/security/AlliancePermissions.java
+++ superid-web-app/src/main/java/cn/superid/webapp/security/AlliancePermissions.java
@@ -1,8 +1,85 @@
 package cn.superid.webapp.security;
 
+import cn.superid.webapp.forms.IdNameNode;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zp on 2016/7/26.
  */
-public enum AlliancePermissions {
-    AddMember,DeleteMember,ViewMember
+public class AlliancePermissions {
+    private static int Default = 0;
+
+    public static int Alliance = Default;
+    public static int ViewAllianceHomepage = 1;
+    public static int EditAllianceInfo = 9;
+
+    public static int Contract = Default;
+    public static int SignContract = 2;
+    public static int ManageContractTemplate = 10;
+    public static int ViewAllContract = 18;
+
+    public static int Fund = Default;
+    public static int CheckAllFundInfo = 3;
+
+    public static int Goods = Default;
+    public static int CheckAllGoodsInfo = 4;
+    public static int ManageGoodsConvertTemplate = 12;
+    public static int ManageBrands = 20;
+
+    public static int Role = Default;
+    public static int AddAllianceRole = 5;
+    public static int InvalidAllianceRole = 13;
+
+    public static int AllocatePermission = 6;
+
+    public static String getAllAlliancePermissions(){
+        List<IdNameNode> rs=new ArrayList<>();
+        IdNameNode childNode ;
+        IdNameNode parentNode;
+
+        parentNode =new IdNameNode(Alliance,"盟信息");
+        rs.add(parentNode);
+        childNode = new IdNameNode(ViewAllianceHomepage,"查看盟主页");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(EditAllianceInfo,"修改盟信息");
+        parentNode.getChilds().add(childNode);
+
+        parentNode =new IdNameNode(Contract,"合同");
+        rs.add(parentNode);
+        childNode = new IdNameNode(SignContract,"签字权限");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(ManageContractTemplate,"管理合同模板");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(ViewAllContract,"查看所有合同");
+        parentNode.getChilds().add(childNode);
+
+        parentNode =new IdNameNode(Fund,"资金");
+        rs.add(parentNode);
+        childNode = new IdNameNode(CheckAllFundInfo,"查看所有资金信息");
+        parentNode.getChilds().add(childNode);
+
+        parentNode =new IdNameNode(Goods,"资产");
+        rs.add(parentNode);
+        childNode = new IdNameNode(CheckAllGoodsInfo,"查看所有资产信息");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(ManageGoodsConvertTemplate,"管理合成模板");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(ManageBrands,"管理商标库");
+        parentNode.getChilds().add(childNode);
+
+        parentNode =new IdNameNode(Role,"角色");
+        rs.add(parentNode);
+        childNode = new IdNameNode(AddAllianceRole,"添加盟成员");
+        parentNode.getChilds().add(childNode);
+        childNode = new IdNameNode(InvalidAllianceRole,"失效盟成员");
+        parentNode.getChilds().add(childNode);
+
+        parentNode =new IdNameNode(AllocatePermission,"分配权限");
+        rs.add(parentNode);
+
+        return JSONObject.toJSONString(rs);
+    }
 }
--- superid-web-app/src/main/java/cn/superid/webapp/service/impl/AffairMemberService.java
+++ superid-web-app/src/main/java/cn/superid/webapp/service/impl/AffairMemberService.java
@@ -1,9 +1,16 @@
 package cn.superid.webapp.service.impl;
 
+import cn.superid.jpa.util.StringUtil;
 import cn.superid.webapp.model.AffairEntity;
 import cn.superid.webapp.model.AffairMemberEntity;
+import cn.superid.webapp.model.PermissionGroupEntity;
+import cn.superid.webapp.security.AffairPermissionRoleType;
 import cn.superid.webapp.service.IAffairMemberService;
+import cn.superid.webapp.utils.TimeUtil;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 
 /**
  * Created by xiaofengxu on 16/9/2.
@@ -13,12 +20,77 @@ public class AffairMemberService implements IAffairMemberService {
     @Override
     public AffairMemberEntity addMember(Long allianceId,Long affairId, Long roleId,  String permissions,long permissionGroupId) {
         AffairMemberEntity affairMemberEntity = new AffairMemberEntity();
+        affairMemberEntity.setAllianceId(allianceId);
         affairMemberEntity.setAffairId(affairId);
         affairMemberEntity.setRoleId(roleId);
         affairMemberEntity.setPermissions(permissions);
         affairMemberEntity.setPermissionGroupId(permissionGroupId);
         affairMemberEntity.setAllianceId(allianceId);
+        affairMemberEntity.setCreateTime(TimeUtil.getCurrentSqlTime());
+        affairMemberEntity.setModifyTime(TimeUtil.getCurrentSqlTime());
         affairMemberEntity.save();
         return affairMemberEntity;
+    }
+
+    public boolean allocateAffairMemberPermissionGroup(Long affairId,Long allianceId,Long toRoleId, Long permissionGroupId) throws Exception {
+        AffairMemberEntity affairMemberEntity = AffairMemberEntity.dao.partitionId(allianceId).eq("affairId", affairId).eq("roleId", toRoleId).selectOne();
+        if (affairMemberEntity == null) {
+            throw new Exception("找不到该事务成员");
+        }
+
+        if (permissionGroupId == null) {
+            throw new Exception("请选择权限组");
+        }
+        /*
+        if ((permissionGroupId.longValue() > 0) || (permissionGroupId.longValue() < 6)) {
+            Iterator it = AffairPermissionRoleType.roles.keySet().iterator();
+            while (it.hasNext()) {
+                Long key = (Long) it.next();
+                if (key.longValue() == permissionGroupId) {
+                    affairMemberEntity.setPermissionGroupId(permissionGroupId);
+                    affairMemberEntity.setPermissions("");
+                    affairMemberEntity.update();
+                    return true;
+                }
+            }
+        }
+         */
+        affairMemberEntity.setPermissionGroupId(permissionGroupId);
+        affairMemberEntity.setPermissions("");
+        affairMemberEntity.update();
+        return true;
+    }
+
+    @Override
+    public boolean modifyAffairMemberPermissions(Long allianceId, Long affairId, Long toRoleId, String permissions) throws Exception {
+        AffairMemberEntity affairMemberEntity = AffairMemberEntity.dao.partitionId(allianceId).eq("affairId", affairId).eq("roleId", toRoleId).selectOne();
+        if (affairMemberEntity == null) {
+            throw new Exception("找不到该事务成员");
+        }
+
+        if (StringUtil.isEmpty(permissions)) {
+            throw new Exception("请选择正确的权限");
+        }
+        affairMemberEntity.setPermissions(permissions);
+        affairMemberEntity.update();
+        return true;
+    }
+
+    @Override
+    public PermissionGroupEntity addPermissionGroup(Long allianceId,Long affairId, String name, String permissions) throws Exception {
+        AffairEntity affairEntity = AffairEntity.dao.partitionId(allianceId).findById(affairId);
+        if(affairEntity == null){
+            throw new Exception("找不到该事务");
+        }
+        if (StringUtil.isEmpty(permissions)) {
+            throw new Exception("请选择正确的权限");
+        }
+        PermissionGroupEntity permissionGroupEntity = new PermissionGroupEntity();
+        permissionGroupEntity.setName(name);
+        permissionGroupEntity.setAffairId(affairId);
+        permissionGroupEntity.setPermissions(permissions);
+        permissionGroupEntity.setCreateTime(TimeUtil.getCurrentSqlTime());
+        permissionGroupEntity.save();
+        return null;
     }
 }
