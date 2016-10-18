4d5c6cc52f5af812e28c3e28fe0cd37da0128403
xmc1993 <1170143845@qq.com>
2016-10-18 16:15:34 +0800

    zookeeper

---
+++ superid-web-app/src/test/java/ZookeeperTest.java
@@ -0,0 +1,19 @@
+import cn.superid.webapp.zookeeper.Zookeeper;
+import org.apache.zookeeper.KeeperException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by xmc1993 on 16/10/18.
+ */
+public class ZookeeperTest {
+
+    @Test
+    public void testZookeeper() throws IOException, KeeperException, InterruptedException {
+        Zookeeper zookeeper = new Zookeeper();
+        zookeeper.connect();
+        zookeeper.watchBackendNodeChange();
+        zookeeper.watchConnectorNodeChange();
+    }
+}
