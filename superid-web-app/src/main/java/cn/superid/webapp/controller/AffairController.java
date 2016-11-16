1544d9268627e84d985de299ed0b076b0cc6f92e
jizhenya <844094462@qq.com>
2016-11-16 13:18:53 +0800

    affair

--- superid-web-app/src/main/java/cn/superid/webapp/controller/AffairController.java
+++ superid-web-app/src/main/java/cn/superid/webapp/controller/AffairController.java
@@ -161,7 +161,7 @@ public class AffairController {
 
 
     @ApiOperation(value = "得到事务树",response = String.class,notes = "拥有权限")
-    @RequestMapping(value = "/get_tree", method = RequestMethod.GET)
+    @RequestMapping(value = "/get_tree", method = RequestMethod.POST)
     @RequiredPermissions()
     public SimpleResponse getTree(Long allianceId) {
         if(allianceId == null){
