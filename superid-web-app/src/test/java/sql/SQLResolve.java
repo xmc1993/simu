d29ef1737754ecd61a3aa378d068e3c04a366de2
jizhenya <844094462@qq.com>
2016-12-07 10:55:51 +0800

    脏数据清除方法

--- superid-web-app/src/test/java/sql/SQLResolve.java
+++ superid-web-app/src/test/java/sql/SQLResolve.java
@@ -33,4 +33,20 @@ public class SQLResolve {
             u.update();
         }
     }
+
+    @Test
+    public void cleanDirtyData(){
+        //用法:打开阿里云sql,直接按顺序复制运行,条件自己变
+        //第一步,清除脏affair_user
+        StringBuilder cleanAffairUser = new StringBuilder("delete from affair_user where affair_id in (select id from affair where alliance_id in (select id from alliance where name = '12'))");
+        //第二步,清除脏affair_member
+        StringBuilder cleanAffairMember = new StringBuilder("delete from affair_member where affair_id in (select id from affair where alliance_id in (select id from alliance where name = '12'))");
+        //第三步,清除脏的affair
+        StringBuilder cleanAffair = new StringBuilder("delete from affair  where alliance_id in (select id from alliance where name = '12')");
+        //第四步,清除脏alliance
+        StringBuilder cleanAlliance = new StringBuilder("delete from alliance where name = '12'");
+
+    }
+
+
 }
