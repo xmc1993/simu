50a50beb37329d4788dc76b857396e4fb95510e6
jizhenya <844094462@qq.com>
2016-12-07 14:21:20 +0800

    修复affair_user表脏数据

--- superid-web-app/src/test/java/sql/SQLResolve.java
+++ superid-web-app/src/test/java/sql/SQLResolve.java
@@ -1,6 +1,7 @@
 package sql;
 
 import cn.superid.jpa.util.ParameterBindings;
+import cn.superid.webapp.model.AffairUserEntity;
 import cn.superid.webapp.model.UserEntity;
 import cn.superid.webapp.service.IAffairService;
 import org.junit.Test;
@@ -48,6 +49,17 @@ public class SQLResolve {
         //第四步,清除脏alliance
         StringBuilder cleanAlliance = new StringBuilder("delete from alliance where name = '12'");
 
+    }
+
+    @Test
+    public void fixAffairUser(){
+        StringBuilder sb = new StringBuilder("select a.id as user_id , b.id as role_id , c.id as affair_id , c.alliance_id as alliance_id from user a join role b join affair c on a.personal_role_id = b.id and b.belong_affair_id = c.id");
+        ParameterBindings p = new ParameterBindings();
+        List<AffairUserEntity> affairUserEntities = AffairUserEntity.dao.findList(sb.toString(),p);
+        for(AffairUserEntity a : affairUserEntities){
+            a.setIsStuck(false);
+            a.save();
+        }
     }
 
 
