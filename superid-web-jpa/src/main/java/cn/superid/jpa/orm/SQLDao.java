bda88c3d7272d7cce88c0e37364abfac77b49c99
jizhenya <844094462@qq.com>
2016-12-15 08:54:13 +0800

    bug修复

--- superid-web-jpa/src/main/java/cn/superid/jpa/orm/SQLDao.java
+++ superid-web-jpa/src/main/java/cn/superid/jpa/orm/SQLDao.java
@@ -39,7 +39,7 @@ public class SQLDao {
             "on a.id = b.affair_id ";
 
     //查找公告
-    public static String SEARCH_ANNOUNCEMENT = "select a.id , a.modify_time from (select id,creator_id,modify_time,title from announcement where alliance_id = ? and affair_id = ? ) a " +
+    public static String SEARCH_ANNOUNCEMENT = "select a.id as announcementId , a.modify_time , a.affair_id from (select id,creator_id,modify_time,title,affair_id from announcement where alliance_id = ? and affair_id = ? ) a " +
             "join ( select id , user_id , title from role where alliance_id = ? ) b " +
             "join ( select id , username from user ) c " +
             "on a.creator_id = b.id and b.user_id = c.id where a.title like ? or b.title = ? or c.username = ? ";
