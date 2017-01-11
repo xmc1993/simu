53794d95b666e651c27b6e4dee15daf7ec8cd582
jizhenya <844094462@qq.com>
2017-01-11 13:22:22 +0800

    bug

--- superid-web-app/src/main/java/cn/superid/webapp/controller/UserController.java
+++ superid-web-app/src/main/java/cn/superid/webapp/controller/UserController.java
@@ -25,10 +25,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
